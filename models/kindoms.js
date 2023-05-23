c9772e605500f8e3b22cb232e776c6adf4938c8f
()gres <serdarsaryjayev2002@gmail.com>
2023-05-23 13:02:33 +0500

    add kindom to db

--- models/kindoms.js
+++ models/kindoms.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class kindoms extends Model {
+  class Kindoms extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  kindoms.init({
-    name: DataTypes.STRING
+  Kindoms.init({
+    name: DataTypes.STRING,
+    img: DataTypes.STRING,
+    body: DataTypes.TEXT
   }, {
     sequelize,
-    modelName: 'kindoms',
+    modelName: 'Kindoms',
   });
-  return kindoms;
+  return Kindoms;
 };
