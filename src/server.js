ec28abd58f7658386b597458ce8f857af867dc68
()gres <serdarsaryjayev2002@gmail.com>
2023-05-23 13:03:21 +0500

    Update

--- src/server.js
+++ src/server.js
@@ -3,14 +3,20 @@ const app = express()
 const models = require('../models')
 require('dotenv').config()
 
+
+app.use((req, res) => {
+    res.status(400).send(404)
+})
+
 app.listen(process.env.port, async () => {
     console.log('Server started')
-    try {
-        await models.sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+    models.sequelize.sync()
+    // try {
+    //     await models.sequelize.authenticate();
+    //     console.log('Connection has been established successfully.');
+    // } catch (error) {
+    //     console.error('Unable to connect to the database:', error);
+    // }
 
 })
 
