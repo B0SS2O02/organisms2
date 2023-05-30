6639089166926d515d66c8dc475161280611c7d2
()gres <serdarsaryjayev2002@gmail.com>
2023-05-30 09:05:25 +0500

    Many edits

--- src/server.js
+++ src/server.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const app = express()
-const models = require('../models')
+const models = require('../models')0
+const swaggerUi = require("swagger-ui-express")
+const swaggerDoc = require("./swagger");
+
+
 const Admin = require('../routers/admin.router.js')
+const AdminCategory = require('../routers/admin.category.router')
+const AdminOrganism = require('../routers/admin.organism.router')
+const Organism = require('../routers/organism.router.js')
+
 require('dotenv').config()
 
 
-
+app.use(express.json())
 
 app.use('/public', express.static('public'))
 
 app.set('view engine', 'ejs');
 
+app.use('/admin/category', AdminCategory)
+
+app.use("/admin/organism", AdminOrganism)
+
+app.use('/api/organism', Organism)
+
 app.use('/admin', Admin)
 
-app.use((req, res) => {
-    res.status(400).send(404)
-})
+
+
+// app.use((req, res) => {
+//     res.status(400).send(404)
+// })
 
 app.listen(process.env.port, async () => {
     console.log('Server started')
